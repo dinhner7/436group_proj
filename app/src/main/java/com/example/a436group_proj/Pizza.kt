26e6d26c98ece78483e6cb7857215f1e008fef6a
flama <dnhuynh723@gmail.com>
2024-11-08 14:34:15 -0500

    Customize progress

--- app/src/main/java/com/example/a436group_proj/Pizza.kt
+++ app/src/main/java/com/example/a436group_proj/Pizza.kt
@@ -1,6 +1,15 @@
 package com.example.a436group_proj
 
 class Pizza {
+    private var size: Int = 1           //0,1,2 = small, medium, large
     private var totalCost: Float = 0f
+    private var numPizzas: Int = 0
+
+    //constant array of toppings and crust
+    private val toppings: Array<String> =
+        arrayOf("pepperoni", "sardines", "sausage", "bacon", "mushrooms",
+            "peppers", "pineapple", "basil", "spinach", "olives")
+    private val crustTypes: Array<String> = arrayOf("normal", "thin crust", "garlic butter")
+
 
 }
