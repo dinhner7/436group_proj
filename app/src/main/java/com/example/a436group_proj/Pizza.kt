bbc062141d7bf8c53fa8b85e16631ca837e9a759
flama <dnhuynh723@gmail.com>
2024-11-13 18:48:59 -0500

    Customize progress

--- app/src/main/java/com/example/a436group_proj/Pizza.kt
+++ app/src/main/java/com/example/a436group_proj/Pizza.kt
@@ -4,24 +4,32 @@ class Pizza {
     private var size: Int = 1           //0,1,2 = small, medium, large
     private var totalCost: Float = 0f
     private var numPizzas: Int = 1
+    private var toppings: ArrayList<String> = ArrayList()
 
     //constant array of toppings, crust, cheese
-    private val toppings: Array<String> =
+    private val toppingTypes: Array<String> =
         arrayOf("pepperoni", "sardines", "sausage", "bacon", "mushrooms",
             "peppers", "pineapple", "basil", "olives", "spinach")
-    private val crustTypes: Array<String> = arrayOf("normal", "thin crust", "garlic butter")
+    private val crustTypess: Array<String> = arrayOf("normal", "thin crust", "garlic butter")
     private val cheeseTypes: Array<String> = arrayOf("mozzarella", "swiss", "parmesan", "none")
 
-    fun setSize(s : Int) {
+    fun setSize(s: Int) {
         size = s
     }
 
-    fun setNumPizzas(num : Int) {
+    fun setNumPizzas(num: Int) {
         numPizzas = num
     }
 
-    fun getTotal() : Float {
+    fun getTotalCost(): Float {
         return totalCost
     }
 
+    fun addTopping(str: String): Boolean{
+        if(!toppings.contains(str)){
+            toppings.add(str)
+            return true
+        }
+        return false
+    }
 }
