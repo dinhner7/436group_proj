ea922dfece58bd4d9dcd8fe646b97e407397d927
Jordan Hoffmann <jhoff721@umd.edu>
2024-11-13 10:07:51 -0500

    Change compileSdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.a436group_proj"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.a436group_proj"
